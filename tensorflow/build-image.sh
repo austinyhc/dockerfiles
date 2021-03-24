e6add0ea73a0479bcc833d1b587b267fb25dfb31
Austin Chen <austin.yhc@gmail.com>
2021-03-24 10:42:00 +0800

    Add pyyaml and argh

--- tensorflow/build-image.sh
+++ tensorflow/build-image.sh
@@ -3,9 +3,19 @@
 IMAGE=${IMAGE:-austinyhc/tensorflow}
 SHORT_SHA=$(git rev-parse --short HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-VERSION=${VERSION:-${SHORT_SHA}}
+VERSION=${VERSION:-latest}
 
-docker image build \
-	--build-arg BUILD_DATE="${DATE}" \
+docker buildx create --name mbuilder
+docker buildx use mbuilder
+docker buildx ls
+docker buildx inspect --bootstrap
+
+docker buildx build \
+    --platform linux/amd64,linux/arm64,linux/arm/v7 \
+    --build-arg BUILD_DATE="${DATE}" \
     --build-arg VCS_REF="${SHORT_SHA}" \
-    -t "${IMAGE}:${VERSION}" .
+    -t "${IMAGE}:${VERSION}" \
+    --push .
+
+docker buildx rm mbuilder
+
