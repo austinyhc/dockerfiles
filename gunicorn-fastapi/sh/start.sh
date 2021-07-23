79b0c75fdc86b1e028518d7fa0835a1758cd75b1
Austin Chen <austin.yhc@gmail.com>
2021-07-23 16:59:54 +0800

    :ice_cream: Change the path in uvicorn command

--- gunicorn-fastapi/sh/start.sh
+++ gunicorn-fastapi/sh/start.sh
@@ -3,7 +3,7 @@ set -e
 
 [ -d "/sntz" ] && pip install -e sntz
 
-DEFAULT_MODULE_NAME=app.main
+DEFAULT_MODULE_NAME=sntz.apis.main
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
@@ -19,7 +19,7 @@ export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
 export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
-PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
+PRE_START_PATH=${PRE_START_PATH:-sntz/sntz/apis/prestart.sh}
 echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
