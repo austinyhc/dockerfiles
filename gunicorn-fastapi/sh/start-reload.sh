42a6c062553a1d03af4a8e938248d55b4c4e1ddd
Austin Chen <austin.yhc@gmail.com>
2021-07-23 20:03:48 +0800

    :ice_cream: Change the path in uvicorn command (#11)

--- gunicorn-fastapi/sh/start-reload.sh
+++ gunicorn-fastapi/sh/start-reload.sh
@@ -3,7 +3,7 @@ set -e
 
 [ -d "/sntz"  ] && pip install -e sntz
 
-DEFAULT_MODULE_NAME=app.main
+DEFAULT_MODULE_NAME=sntz.apis.main
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
@@ -13,7 +13,7 @@ PORT=${PORT:-80}
 LOG_LEVEL=${LOG_LEVEL:-debug}
 
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
-PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
+PRE_START_PATH=${PRE_START_PATH:-/sntz/sntz/apis/prestart.sh}
 echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
@@ -23,4 +23,4 @@ else
 fi
 
 # Start Uvicorn with live reload
-exec uvicorn --reload --reload-dir /app --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
+exec uvicorn --reload --reload-dir /sntz/sntz/apis --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
