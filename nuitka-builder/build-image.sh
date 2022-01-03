525f2a92084099aa058d5b30285f979a4673d5c9
Austin Chen <austin.yhc@gmail.com>
2022-01-03 22:32:30 +0800

    :baby_bottle: First check-in

---
+++ nuitka-builder/build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE=${IMAGE:-austinyhc/nuitka-builder}
+SHORT_SHA=$(git rev-parse --short HEAD)
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+VERSION=${VERSION:-${SHORT_SHA}}
+
+docker build \
+    --build-arg BUILD_DATE="${DATE}" \
+    --build-arg VCS_REF="${SHORT_SHA}" \
+    -t "${IMAGE}:${VERSION}" .
+
+docker push "${IMAGE}:${VERSION}"
