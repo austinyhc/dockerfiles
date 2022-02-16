ce83398f928ff1e47fa3f5dfdd7b3b69d2c57548
Austin Chen <austin.yhc@gmail.com>
2022-02-16 14:23:48 +0800

    :stop_sign: Add to verify STNZ_GH_TOKEN in build script

--- evirdeno/build-image.sh
+++ evirdeno/build-image.sh
@@ -5,9 +5,17 @@ SHORT_SHA=$(git rev-parse --short HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 VERSION=${VERSION:-${SHORT_SHA}}
 
+if [[ ${SNTZ_GH_TOKEN:-"unset"} == "unset"  ]]; then
+    echo "Please set a security token to your local env variable by 'export SNTZ_GH_TOKEN=...'";
+    exit 2;
+fi
+
+SNTZ_GH_TOKEN=${SNTZ_GH_TOKEN}
+
 docker build \
     --build-arg BUILD_DATE="${DATE}" \
     --build-arg VCS_REF="${SHORT_SHA}" \
+    --build-arg GH_TOKEN="${SNTZ_GH_TOKEN}" \
     -t "${IMAGE}:${VERSION}" .
 
 docker push "${IMAGE}:${VERSION}"
